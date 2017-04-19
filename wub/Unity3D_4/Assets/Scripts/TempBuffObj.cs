6ebebd3e3d6e0ebdd457b8cd18fcd0e070c18340
Johannes Rasinkangas <jrasinkangas@mica.edu>
2017-04-18 21:57:04 -0400

    desktop update
    animations broke. dont know why. commit before fixing it.

---
+++ wub/Unity3D_4/Assets/Scripts/TempBuffObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempBuffObj : MonoBehaviour {
+    
+   public string p1Tag;
+   // public string p2Tag;
+    BS_Marker_Manager dmgCont;
+    BS_Main_Health health;
+    new Animator animation;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == p1Tag)
+        {
+            Debug.Log("Power Up Triggered");
+            animation.Play("PowerUp");
+
+        }
+
+ 
+    }
+
+
+
+}
